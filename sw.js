35f2bb13109398790891175122cc7a57e951d382
BackRoad-Inc <dominic.calandro1991@yahoo.com>
2026-09-08 13:00:18 -0500

    Add PWA shell: manifest, service worker, icon

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open("rb-v1").then(function(c){ return c.addAll(["./","./index.html","./manifest.json"]); }));
+});
+self.addEventListener("fetch", function(e){
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
+});
